08b23fde6f8d10af68d394244841b655890091b3
Chen Weiguang <chen.weiguang@gmail.com>
2017-04-12 19:58:06 +0800

    Added some sample index.html + app.js using vue.js.

---
+++ site/app.js
@@ -0,0 +1,26 @@
+var Main = {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
+  }
+}
+
+var Ctor = Vue.extend(Main)
+new Ctor().$mount('#app')
